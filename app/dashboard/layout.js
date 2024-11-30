30169aec3b8e51978bd0610b66150231e920abfe
Laxesh <laxeshhirapara@gmail.com>
2024-11-30 09:33:11 +0530

    error slove

--- app/dashboard/layout.js
+++ app/dashboard/layout.js
@@ -3,9 +3,7 @@ import Footer from "@/components/ui/Footer";
 export default function DashboardLayout({ children }) {
   return (
     <>
-      <body>
-        <div className="">{children}</div>
-      </body>
+      {children}
       <Footer />
     </>
   );
